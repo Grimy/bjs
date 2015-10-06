1fff420f103b9a43cff6dfa818a9b631efb229a4
Victor Adam <victor.adam@cofelyineo-gdfsuez.com>
2015-10-06 11:19:05 +0200

    Use NaNs instead of 0.0f for ~10% performance gain

--- bjs.c
+++ bjs.c
@@ -15,7 +15,10 @@
 	} dest = save; result / ++cards_left; })
 
 #define max(a, b) ((a) > (b) ? (a) : (b))
+#define isnan(x) ((x) != (x)) // faster than the one from math.h
 
+/* Returns 1 if a > b, -1 if a < b, and 0 if a == b.
+ * Same effect as (a > b) - (a < b), but about 2% faster. */
 static inline double cmp(long a, long b) {
 	double result;
 	__asm__ volatile("cmpq %2, %1; seta %b1; sbb $0, %k1; cvtsi2sd %k1, %0"
@@ -23,10 +26,10 @@ static inline double cmp(long a, long b) {
 	return result;
 }
 
-enum { SUM = 0, VALUE = 11, CAN_SPLIT = 12, WAS_SPLIT = 13 };
-static long cache[CACHE_SIZE][14];
-static double hand_cache[10][CACHE_SIZE][5] = {{{0}}};
-static double bank_cache[CACHE_SIZE] = {0};
+enum { VALUE = 0, CAN_SPLIT = 11, WAS_SPLIT = 12 };
+static long cache[CACHE_SIZE][13];
+static double hand_cache[10][CACHE_SIZE][5];
+static double bank_cache[CACHE_SIZE] = {1};
 static long hand = 1;
 static long bank = 1;
 
@@ -36,7 +39,7 @@ static int cards_left = 52 * NDECKS;
 static long bank_first = 0;
 
 static double eval_bank(void) {
-	if (bank_cache[bank] == .0)
+	if (isnan(bank_cache[bank]))
 		bank_cache[bank] = cache[bank][VALUE] >= 17
 			? cmp(cache[hand][VALUE], cache[bank][VALUE])
 			: weighted_avg(bank, eval_bank());
@@ -55,7 +58,8 @@ static double eval_hand(long moves) {
 		return BLACKJACK_PAYS * (double) (cards_left -
 			deck[(bank_first == 1) ? 10 : bank_first == 10]) / cards_left;
 
-	memset(bank_cache + 1, 0, sizeof(bank_cache) - sizeof(double));
+	memset(bank_cache + 1, 255, sizeof(bank_cache) - sizeof(*bank_cache));
+
 	exp[0] = eval_bank();
 	if (cache[hand][VALUE] > 18)
 		return exp[4] = exp[3] = exp[2] = exp[1] = exp[0];
@@ -86,8 +90,8 @@ static void print_strat(long hand_first, long hand_snd) {
 	hand = cache[hand][hand_first];
 	hand = cache[hand][hand_snd];
 	printf("%2ld %2ld", hand_first, hand_snd);
-	for (bank_first = 2; bank_first < 12; ++bank_first) {
-		double *exp = hand_cache[bank_first == 11 ? 0 : bank_first - 1][hand];
+	for (bank_first = 1; bank_first < 11; ++bank_first) {
+		double *exp = hand_cache[bank_first % 10][hand];
 		long max = 0;
 		for (long i = 1; i < 5; i++)
 			if (exp[i] > exp[max])
@@ -97,38 +101,35 @@ static void print_strat(long hand_first, long hand_snd) {
 	printf("\n");
 }
 
-static void fill_cache(long prev_hash, long new_card, long ordered) {
+static void fill_cache(long prev_hash, long sum, long new_card) {
 	static long count = 0;
 	static long hand[11] = {0};
 	
-	if (!ordered) {
-		long hash = 1;
-		for (long j = 1; j < 11; ++j)
-			for (long k = 0; k < hand[j]; ++k)
-				hash = cache[hash][j];
-		cache[prev_hash][new_card] = hash;
-		return;
-	}
 	long hash = ++count;
-	long sum = cache[prev_hash][SUM] + new_card;
 	cache[prev_hash][new_card] = hash;
-	cache[hash][SUM] = sum;
 	cache[hash][VALUE] = sum + 10 * (hand[1] && sum <= 11); // soft hand
 	cache[hash][VALUE] += hand[1] && hand[10] && sum == 11; // blackjack
 	cache[hash][CAN_SPLIT] = sum % 2 == 0 && hand[sum / 2] == 2 ? sum / 2 : 0;
 	cache[hash][WAS_SPLIT] = sum < 11 && hand[sum] ? sum : 0;
 
-	for (long i = 1; i < 11 && sum + i <= 21; ++i) {
+	long i = 1;
+	for (; i < new_card && sum + i <= 21; ++i) {
+		long new_hash = 1;
+		for (long j = 1; j < 11; ++j)
+			for (long k = 0; k < hand[j] + (j == i); ++k)
+				new_hash = cache[new_hash][j];
+		cache[hash][i] = new_hash;
+	}
+
+	for (; i < 11 && sum + i <= 21; ++i) {
 		++hand[i];
-		fill_cache(hash, i, i >= new_card);
+		fill_cache(hash, sum + i, i);
 		--hand[i];
 	}
 }
 
 int main(void) {
-	fill_cache(0, 0, 1);
-	bank_cache[0] = 1;
-
+	fill_cache(0, 0, 0);
 	double expected_gain = weighted_avg(bank,
 		bank_first = i;
 		weighted_avg(hand, weighted_avg(hand, eval_hand(4)))
