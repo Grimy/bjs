0eafbcaf8eaf22e828469ee21717cf158f6f507c
Victor Adam <victor.adam@cofelyineo-gdfsuez.com>
2015-10-13 16:30:59 +0200

    Refactoring

--- bjs.c
+++ bjs.c
@@ -6,7 +6,7 @@
 #define DOUBLE_AFTER_SPLIT 1
 #define BLACKJACK_PAYS 1.5
 
-#define CACHE_SIZE (3084)
+#define CACHE_SIZE (3083)
 
 #define weighted_avg(dest, code) __extension__ ({ \
 	cards_left--; long save = dest; double result = 0; \
@@ -26,17 +26,16 @@ static inline double cmp(long a, long b) {
 	return result;
 }
 
-enum { VALUE = 0, CAN_SPLIT = 11, WAS_SPLIT = 12 };
-static long cache[CACHE_SIZE][13];
-static double hand_cache[12][CACHE_SIZE + 1000][7];
+enum { VALUE = 0, CAN_SPLIT = 11 };
+static long cache[CACHE_SIZE][12];
+static double hand_cache[10][CACHE_SIZE][5];
 static double bank_cache[CACHE_SIZE] = {1};
-static long hand = 1;
-static long bank = 1;
+static long hand;
+static long bank;
 
-__extension__ static long deck[11] = { [1 ... 9] = 4 * NDECKS, 4 * 4 * NDECKS};
+__extension__ static long deck[11] = {[1 ... 9] = 4 * NDECKS, 4 * 4 * NDECKS};
 
 static int cards_left = 52 * NDECKS;
-static long bank_first = 0;
 
 static double eval_bank(void) {
 	if (isnan(bank_cache[bank]))
@@ -47,53 +46,40 @@ static double eval_bank(void) {
 }
 
 static double eval_hand(long moves) {
-	double *exp = hand_cache[bank_first - 1][hand];
+	double *exp = hand_cache[bank - 1][hand];
 	if (exp[moves])
 		return exp[moves];
 
-	if (!hand)
+	if (!hand) // Busted!
 		return exp[4] = exp[3] = exp[2] = exp[1] = exp[0] = -1;
 
 	memset(bank_cache + 1, 255, sizeof(bank_cache) - sizeof(*bank_cache));
-
 	exp[0] = eval_bank() * (cache[hand][VALUE] == 22 ? BLACKJACK_PAYS : 1);
 
-	if (cache[hand][VALUE] == 22) {
-		printf("Old: %f, %ld, %ld\n", BLACKJACK_PAYS * (double) (cards_left - deck[(bank_first == 1) ? 10 : bank_first == 10]) / cards_left, hand, bank);
-		return BLACKJACK_PAYS * (double) (cards_left - deck[(bank_first == 1) ? 10 : bank_first == 10]) / cards_left;
-	}
-
-	if (cache[hand][VALUE] == 22) {
-		printf("New: %f\n", exp[0]);
+	if (cache[hand][VALUE] > 18)
 		return exp[4] = exp[3] = exp[2] = exp[1] = exp[0];
-	}
 
-	long das = cache[hand][WAS_SPLIT] && DOUBLE_AFTER_SPLIT;
+	long das = hand < 11 && DOUBLE_AFTER_SPLIT;
 	double hit = weighted_avg(hand, cache[hand][CAN_SPLIT] ? .0 : eval_hand(1 + das));
 	double dbl = weighted_avg(hand, cache[hand][CAN_SPLIT] ? .0 : 2 * eval_hand(0));
-	double split = -2;
-	if (cache[hand][CAN_SPLIT]) {
-		long save = hand;
-		hand = cache[1][cache[hand][CAN_SPLIT]];
-		split = 2 * eval_hand(1);
-		hand = save;
-	}
+	double split = hand_cache[bank - 1][cache[hand][CAN_SPLIT]][1];
 
-	double ratio = (cards_left - 2.0 * deck[cache[hand][WAS_SPLIT]]) / cards_left;
+	double ratio = (cards_left - (hand < 11 ? 2.0 * deck[hand] : 0.0)) / cards_left;
+	printf("left: %ld\n", hand < 11 ? deck[hand] : 0);
 	exp[1] = max(exp[0], hit / ratio);
 	exp[2] = max(exp[1], dbl / ratio);
 	exp[3] = max(exp[2], -.5);
 	exp[4] = max(exp[2], split);
+
 	return exp[moves];
 }
 
 static void print_strat(long hand_first, long hand_snd) {
 	char repr[] = "SHDRP";
-	hand = 1;
-	hand = cache[hand][hand_first];
+	hand = cache[0][hand_first];
 	hand = cache[hand][hand_snd];
 	printf("%2ld %2ld", hand_first, hand_snd);
-	for (bank_first = 1; bank_first < 11; ++bank_first) {
+	for (long bank_first = 1; bank_first < 11; ++bank_first) {
 		double *exp = hand_cache[bank_first % 10][hand];
 		long max = 0;
 		for (long i = 1; i < 5; i++)
@@ -104,37 +90,33 @@ static void print_strat(long hand_first, long hand_snd) {
 	printf("\n");
 }
 
-static void fill_cache(long prev_hash, long sum, long new_card) {
-	static long count = 0;
-	static long hand[11] = {0};
+static void fill_cache(long hash, long sum, long new_card) {
+	static long count;
+	static long hand[11];
 	
-	long hash = ++count;
-	long i = 1;
-
-	cache[prev_hash][new_card] = hash;
-	cache[hash][VALUE] = sum + 10 * (hand[1] && sum <= 11); // soft hand
-	cache[hash][VALUE] += hand[1] && hand[10] && sum == 11; // blackjack
+	long soft = hand[1] && sum <= 11;
+	cache[hash][VALUE] = sum + 10 * soft + (soft && hand[10]);
 	cache[hash][CAN_SPLIT] = sum % 2 == 0 && hand[sum / 2] == 2 ? sum / 2 : 0;
-	cache[hash][WAS_SPLIT] = sum < 11 && hand[sum] ? sum : 0;
 
-	for (; i < new_card && sum + i <= 21; ++i) {
-		cache[hash][i] = 1;
+	for (long i = 1; i < new_card && sum + i <= 21; ++i)
 		for (long j = 1; j < 11; ++j)
 			for (long k = 0; k < hand[j] + (j == i); ++k)
 				cache[hash][i] = cache[cache[hash][i]][j];
-	}
 
-	for (; i < 11 && sum + i <= 21; ++i) {
+	for (long i = new_card; i < 11 && sum + i <= 21; ++i)
+		cache[hash][i] = ++count;
+
+	for (long i = new_card; i < 11 && sum + i <= 21; ++i) {
 		++hand[i];
-		fill_cache(hash, sum + i, i);
+		fill_cache(cache[hash][i], sum + i, i);
 		--hand[i];
 	}
 }
 
 int main(void) {
-	fill_cache(0, 0, 0);
+	fill_cache(0, 0, 1);
+	weighted_avg(bank, weighted_avg(hand, eval_hand(1)));
 	double expected_gain = weighted_avg(bank,
-		bank_first = i;
 		weighted_avg(hand, weighted_avg(hand, eval_hand(4)))
 	);
 	printf("Expected gain: %f%%\n", expected_gain * 100);
