c3cc0ab98039b8ac7bf6a790a532ab733c78c8a5
Victor Adam <victor.adam@cofelyineo-gdfsuez.com>
2015-10-14 16:49:44 +0200

    fix

--- bjs.c
+++ bjs.c
@@ -46,22 +46,24 @@ static double _eval_bank(long hand, long bank) {
 }
 
 static double _eval_hand(long hand, long bank, long moves) {
+	// Look in the cache
 	double *exp = hand_cache[bank - 1][hand];
 	if (exp[moves])
 		return exp[moves];
 
-	if (!hand) // Busted!
+	// Busted!
+	if (!hand)
 		return exp[4] = exp[3] = exp[2] = exp[1] = exp[0] = -1;
 
+	// Stay
 	memset(bank_cache + 1, 255, sizeof(bank_cache) - sizeof(*bank_cache));
 	exp[0] = eval_bank() * (cache[hand][VALUE] == 22 ? BLACKJACK_PAYS : 1);
-
-	if (!moves)
+	if (moves == 0)
 		return exp[0];
-
 	if (cache[hand][VALUE] > 18)
 		return exp[4] = exp[3] = exp[2] = exp[1] = exp[0];
 
+	// Hit
 	long das = hand < 11 && DOUBLE_AFTER_SPLIT;
 	double hit = mean(hand, cache[hand][CAN_SPLIT] ? .0 : eval_hand(1 + das));
 	double dbl = mean(hand, cache[hand][CAN_SPLIT] ? .0 : 2 * eval_hand(0));
@@ -71,8 +73,7 @@ static double _eval_hand(long hand, long bank, long moves) {
 	exp[1] = max(exp[0], hit / ratio);
 	exp[2] = max(exp[1], dbl / ratio);
 	exp[3] = max(exp[2], -.5);
-	exp[4] = max(exp[2], split);
-
+	exp[4] = max(exp[3], split);
 	return exp[moves];
 }
 
