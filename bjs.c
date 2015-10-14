470560a2f29926799a14ea9fec0655006d2ddd3a
Victor Adam <victor.adam@cofelyineo-gdfsuez.com>
2015-10-14 18:23:26 +0200

    opti: struct hand

--- bjs.c
+++ bjs.c
@@ -9,7 +9,7 @@
 #define CACHE_SIZE (3083)
 
 #define mean(dest, code) __extension__ ({ \
-	cards_left--; double result = 0; long *p = cache[dest]; \
+	cards_left--; double result = 0; long *p = cache[dest].next; \
 	for (long i = 1; i < 11; ++deck[i], ++i) { \
 		long dest = p[i]; result += (double) deck[i]-- * ({code;}); \
 	} result / ++cards_left; })
@@ -28,8 +28,12 @@ static inline double cmp(long a, long b) {
 	return result;
 }
 
-enum { VALUE = 0, CAN_SPLIT = 11 };
-static long cache[CACHE_SIZE][12];
+struct hand {
+	long next[11];
+	long value;
+	long can_split;
+};
+static struct hand cache[CACHE_SIZE];
 static double hand_cache[CACHE_SIZE][5];
 static double bank_cache[CACHE_SIZE] = {1};
 
@@ -39,8 +43,8 @@ static int cards_left = 52 * NDECKS;
 
 static double _eval_bank(long hand, long bank) {
 	if (isnan(bank_cache[bank]))
-		bank_cache[bank] = cache[bank][VALUE] >= 17
-			? cmp(cache[hand][VALUE], cache[bank][VALUE])
+		bank_cache[bank] = cache[bank].value >= 17
+			? cmp(cache[hand].value, cache[bank].value)
 			: mean(bank, eval_bank());
 	return bank_cache[bank];
 }
@@ -57,17 +61,17 @@ static double _eval_hand(long hand, long bank, long moves) {
 
 	// Stay
 	memset(bank_cache + 1, 255, sizeof(bank_cache) - sizeof(*bank_cache));
-	exp[0] = eval_bank() * (cache[hand][VALUE] == 22 ? BLACKJACK_PAYS : 1);
+	exp[0] = eval_bank() * (cache[hand].value == 22 ? BLACKJACK_PAYS : 1);
 	if (moves == 0)
 		return exp[0];
-	if (cache[hand][VALUE] > 18)
+	if (cache[hand].value > 18)
 		return exp[4] = exp[3] = exp[2] = exp[1] = exp[0];
 
 	// Hit
 	long das = hand < 11 && DOUBLE_AFTER_SPLIT;
-	double hit = mean(hand, cache[hand][CAN_SPLIT] ? .0 : eval_hand(1 + das));
-	double dbl = mean(hand, cache[hand][CAN_SPLIT] ? .0 : 2 * eval_hand(0));
-	double split = 2 * hand_cache[cache[hand][CAN_SPLIT]][1];
+	double hit = mean(hand, cache[hand].can_split ? .0 : eval_hand(1 + das));
+	double dbl = mean(hand, cache[hand].can_split ? .0 : 2 * eval_hand(0));
+	double split = 2 * hand_cache[cache[hand].can_split][1];
 
 	double ratio = (cards_left - (hand < 11 ? 2.0 * deck[hand] : 0.0)) / cards_left;
 	exp[1] = max(exp[0], hit / ratio);
@@ -82,27 +86,27 @@ static void fill_cache(long hash, long sum, long new_card) {
 	static long hand[11];
 	
 	long soft = hand[1] && sum <= 11;
-	cache[hash][VALUE] = sum + 10 * soft + (soft && hand[10]);
-	cache[hash][CAN_SPLIT] = sum % 2 == 0 && hand[sum / 2] == 2 ? sum / 2 : 0;
+	cache[hash].value = sum + 10 * soft + (soft && hand[10]);
+	cache[hash].can_split = sum % 2 == 0 && hand[sum / 2] == 2 ? sum / 2 : 0;
 
 	for (long i = 1; i < new_card && sum + i <= 21; ++i)
 		for (long j = 1; j < 11; ++j)
 			for (long k = 0; k < hand[j] + (j == i); ++k)
-				cache[hash][i] = cache[cache[hash][i]][j];
+				cache[hash].next[i] = cache[cache[hash].next[i]].next[j];
 
 	for (long i = new_card; i < 11 && sum + i <= 21; ++i)
-		cache[hash][i] = ++count;
+		cache[hash].next[i] = ++count;
 
 	for (long i = new_card; i < 11 && sum + i <= 21; ++i) {
 		++hand[i];
-		fill_cache(cache[hash][i], sum + i, i);
+		fill_cache(cache[hash].next[i], sum + i, i);
 		--hand[i];
 	}
 }
 
 static void print_strat(long bank, long hand_first, long hand_snd) {
 	char repr[] = "SHDRP";
-	double *exp = hand_cache[cache[hand_first][hand_snd]];
+	double *exp = hand_cache[cache[hand_first].next[hand_snd]];
 	long max = 0;
 	for (long i = 1; i < 5; i++)
 		if (exp[i] > exp[max])
